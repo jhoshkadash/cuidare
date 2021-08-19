b5093e820b750319651b42b55fd38ef6deca8966
Joshua Martins Duarte <jhoshkadash@live.com>
2021-08-19 14:55:59 -0300

    AtendenteVO

---
+++ model.vo/AtendenteVO.java
@@ -0,0 +1,11 @@
+public class AtendenteVO {
+    String nome;
+    private String cpf;
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+        
+}
