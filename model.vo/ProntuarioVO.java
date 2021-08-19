caf33ca7d4823683a6cfc372ffabcb97f900822f
Joshua Martins Duarte <jhoshkadash@live.com>
2021-08-19 16:14:18 -0300

    changes

--- model.vo/ProntuarioVO.java
+++ model.vo/ProntuarioVO.java
@@ -1,4 +1,28 @@
 public class ProntuarioVO {
-    String[] remedios;
-    String[] observacoes;    
+    private String[] remedios;
+    private String[] observacoes;
+    
+    public String[] getRemedios() {
+        return remedios;
+    }
+    public void setRemedios(String[] remedios) {
+        this.remedios = remedios;
+    }
+    public String[] getObservacoes() {
+        return observacoes;
+    }
+    public void setObservacoes(String[] observacoes) {
+        if(observacoes==null)
+        {
+
+        }
+        else  if(observacoes.equals("")) 
+        {
+
+        }
+        else  this.observacoes = observacoes;
+    }
+    }    
+
+    
 }
