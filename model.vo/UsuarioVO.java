46dce6ca5c302676e3cd8b0075e18c313ca2ffd7
LucasPaulinoH <87985909+LucasPaulinoH@users.noreply.github.com>
2021-08-19 16:36:31 -0300

    Update UsuarioVO.java

--- model.vo/UsuarioVO.java
+++ model.vo/UsuarioVO.java
@@ -8,31 +8,34 @@ public class UsuarioVO
     {
         return login;
     }
-    public String getSenha()
-    {
-        return senha;
-    }
+    
     public void setLogin(String login)
     {
         if(login==null)
         {
-
+            System.out.println ("Digite um login: ");
         }
         else  if(login.equals("")) 
         {
-
+            System.out.println ("Digite um login: ");
         }
         else  this.login = login;
     }
+    
+    public String getSenha()
+    {
+        return senha;
+    }
+    
     public void setSenha(String senha)
     {
         if(senha==null)
         {
-
+            System.out.println ("Digite uma senha: ");
         }
         else  if(senha.equals("")) 
         {
-
+            System.out.println ("Digite uma senha: ");
         }
         else  this.senha = senha;
     }
