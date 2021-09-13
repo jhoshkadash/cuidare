a52f3fbfa7fc9a984f2cbc5753052933e7de0d1c
LucasPaulinoH <87985909+LucasPaulinoH@users.noreply.github.com>
2021-09-13 14:48:27 -0300

    Update GerenteVO.java

--- src/Model/VO/GerenteVO.java
+++ src/Model/VO/GerenteVO.java
@@ -1,31 +1,19 @@
 package Model.VO;
 
-
 public class GerenteVO extends UsuarioVO {
     private String nome;
     private Integer id;
-
 
     public GerenteVO(String nome, Integer id) {
         this.nome = nome;
         this.id = id;
     }
 
+    
     public String getNome() {
         return nome;
     }
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        if(id == null){
-            System.out.println("Erro. Gerente sem ID");
-        }
-        else if(id.equals("")){
-            System.out.println("Erro. ID vazio");
-        }
-        else this.id = id;
-    }
+    
     public void setNome(String nome) {
         if(nome == null){
             System.out.println("Erro. Gerente sem nome");
@@ -34,5 +22,20 @@ public class GerenteVO extends UsuarioVO {
             System.out.println("Erro. Nome do gerente vazio");
         }
         else this.nome = nome;
+    }
+    
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    public void setId(Integer id) {
+        if(id == null){
+            System.out.println("Erro. Gerente sem ID");
+        }
+        else if(id.equals("")){
+            System.out.println("Erro. ID vazio");
+        }
+        else this.id = id;
     }
 }
