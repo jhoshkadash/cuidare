c9912bda93aad4d2f3225aaf5de4c5b14761ac58
João Neto <jnetogoncalo@gmail.com>
2021-09-14 10:08:58 -0300

    att

--- src/Model/DAO/Teste.java
+++ src/Model/DAO/Teste.java
@@ -8,9 +8,14 @@ public class teste {
         BaseDAO dao = new BaseDAO();
         AtendenteDAO atendenteDao = new AtendenteDAO();
         AtendenteVO atendenteVo = new AtendenteVO();
+        AtendenteVO atendenteVo2 = new AtendenteVO();
 
         atendenteVo.setCpf("33344455567");
         atendenteVo.setNome("Matheus Cunha");
+        atendenteDao.inserir(atendenteVo);
+        atendenteVo2.setCpf("66677788890");
+        atendenteVo2.setNome("Matue da Silva");
+        atendenteDao.inserir(atendenteVo2);
         atendenteDao.removerByCPF(atendenteVo);} 
 
     
