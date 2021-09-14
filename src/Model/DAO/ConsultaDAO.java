6d4ad5e08da999e598b4ba436c62e1a0b18cee6f
LucasPaulinoH <87985909+LucasPaulinoH@users.noreply.github.com>
2021-09-14 14:46:12 -0300

    criação da ConsultaDAO
    linhas 19 e 20: não foram aplicadas as funções gets pertinentes aos dados calendar

---
+++ src/Model/DAO/ConsultaDAO.java
@@ -0,0 +1,47 @@
+package Model.DAO;
+
+import Model.VO.*;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class ConsultaDAO extends BaseDAO
+{
+    public void inserir(ConsultaVO vo)
+    {
+        conn = getConnection();
+        String sql = "insert into Consulta(Id,Data,Hora,Status,IdMedico,IdPaciente) values (?,?,?,?,?,?)";
+        PreparedStatement ptst;
+        try 
+        {
+            ptst = conn.prepareStatement(sql);
+            ptst.setInt(1, vo.getId());
+            ptst.setNString(2, vo.getData());
+            ptst.setCalendar(3, vo.getHora);
+            ptst.setBoolean(4, vo.isStatus());
+            ptst.setNString(5, vo.getIdMedico());
+            ptst.setNString(6, vo.getIdPaciente());
+            ptst.execute();
+        } 
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public void removerById(ConsultaVO vo)
+    {
+        conn = getConnection();
+        String sql = "delete from Consulta where Id = ?";
+        PreparedStatement ptst;
+        try 
+        {
+            ptst = conn.prepareStatement(sql);
+            ptst.setInt(1, vo.getId());
+            ptst.executeUpdate();
+        } 
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
