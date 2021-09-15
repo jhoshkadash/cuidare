648f90e2b83753b7586280131724cf5fd9289ece
LucasPaulinoH <87985909+LucasPaulinoH@users.noreply.github.com>
2021-09-14 21:15:45 -0300

    att editar

--- src/Model/DAO/AtendenteDAO.java
+++ src/Model/DAO/AtendenteDAO.java
@@ -4,10 +4,12 @@ import Model.VO.*;
 import java.util.*;
 import java.sql.*;
 
-public class AtendenteDAO extends BaseDAO {
+public class AtendenteDAO extends BaseDAO 
+/* declaração de classe para a criação de Atendentes DAO implementados a MariaDB */
+{
     // criação do método de inserção de atendentes ao MariaDB
     public void inserir(AtendenteVO vo) {
-        conn = getConnection();
+        conn = getConnection(); //conexão estabelecida
         String sql = "insert into Atendente(nome,cpf) values (?,?)"; /* passa o comando de inserção em SQL para o DB. */
         /* passa o comando de inserção em SQL para o DB. */
         PreparedStatement ptst;
@@ -23,7 +25,7 @@ public class AtendenteDAO extends BaseDAO {
 
     // criação do método de remorção de atendentes 
     public void removerByCPF(AtendenteVO vo) {
-        conn = getConnection();
+        conn = getConnection(); //conexão estabelecida
         String sql = "delete from Atendente where cpf = ?"; /* passa o comando de remoção em SQL para o DB. */
         PreparedStatement ptst;
         try {
@@ -37,7 +39,7 @@ public class AtendenteDAO extends BaseDAO {
 
     // criação do método de listagem de atendentes 
     public List<AtendenteVO> listar() {
-        conn = getConnection();
+        conn = getConnection(); //conexão estabelecida
         String sql = "select * from atendente"; /* comando de listagem em SQL para o DB. */
         Statement st;
         ResultSet rs;
@@ -60,8 +62,8 @@ public class AtendenteDAO extends BaseDAO {
 
     // método de edição dos dados (nome e Id) da tabela Atendente 
     public void editarNome(AtendenteVO vo) {
-        conn = getConnection();
-        String sql = "update atendente set nome = ? where id = ?";
+        conn = getConnection(); //conexão estabelecida
+        String sql = "update atendente set nome = ? where id = ?";  /* comando de edição em SQL para o DB. */
         PreparedStatement psts;
         try {
             psts = conn.prepareStatement(sql);
@@ -76,7 +78,7 @@ public class AtendenteDAO extends BaseDAO {
     // método de edição do dado CPF da tabela Atendente 
     public void editarCpf(AtendenteVO vo) {
         conn = getConnection();
-        String sql = "update atendente set cpf = ? where id = ?";
+        String sql = "update atendente set cpf = ? where id = ?";  /* comando de listagem em SQL para o DB. */
         PreparedStatement psts;
         try {
             psts = conn.prepareStatement(sql);
@@ -87,61 +89,4 @@ public class AtendenteDAO extends BaseDAO {
             e.printStackTrace();
         }
     }
-}
-
-    // Listar Atendentes
-
-    public List<AtendenteVO> listar() {
-        conn = getConnection();
-        String sql = "select * from atendente";
-        Statement st;
-        ResultSet rs;
-        List<AtendenteVO> atendentes = new ArrayList<AtendenteVO>();
-        try {
-            st = conn.createStatement();
-            rs = st.executeQuery(sql);
-            while (rs.next()) {
-                AtendenteVO vo = new AtendenteVO();
-                vo.setCpf(rs.getString("CPF"));
-                vo.setNome(rs.getString("Nome"));
-                vo.setId(rs.getInt("id"));
-                atendentes.add(vo);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return atendentes;
-    }
-
-    // editar nome atendente
-    public void editarNome(AtendenteVO vo) {
-        conn = getConnection();
-        String sql = "update atendente set nome = ? where id = ?";
-        PreparedStatement psts;
-        try {
-            psts = conn.prepareStatement(sql);
-            psts.setString(1, vo.getNome());
-            psts.setInt(2, vo.getId());
-            psts.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // editar cpf atendente
-    public void editarCpf(AtendenteVO vo) {
-        conn = getConnection();
-        String sql = "update atendente set cpf = ? where id = ?";
-        PreparedStatement psts;
-        try {
-            psts = conn.prepareStatement(sql);
-            psts.setString(1, vo.getCpf());
-            psts.setInt(2, vo.getId());
-            psts.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
-
 }
