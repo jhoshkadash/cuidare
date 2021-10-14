3a4a05aabcc17a7b86fdf4dc6d7ea7b41f5f5b28
LucasPaulinoH <lucashsp12@hotmail.com>
2021-10-14 16:53:04 -0300

    criação VIEW

---
+++ src/Model/VIEW/telaLogin.java
@@ -0,0 +1,16 @@
+package Model.VIEW;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Stage;
+
+public class telaLogin extends Application{
+
+    public static void main (String[] args){
+
+    }
+
+    
+}
