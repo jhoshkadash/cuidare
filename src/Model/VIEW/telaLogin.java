4344d4feb432033bfaa183b259d36cad3df2908c
LucasPaulinoH <lucashsp12@hotmail.com>
2021-10-14 17:02:47 -0300

    update VIEW

--- src/Model/VIEW/telaLogin.java
+++ src/Model/VIEW/telaLogin.java
@@ -4,12 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.Stage;
+import javafx.stage.Stage;
 
 public class telaLogin extends Application{
 
     public static void main (String[] args){
         launch();
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        
     }
 
     
