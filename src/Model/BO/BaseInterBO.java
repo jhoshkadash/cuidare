df86e2e3c535c1f6c6cc3a32bf32e8cc1cd0ada9
LucasPaulinoH <lucashsp12@hotmail.com>
2021-10-21 19:07:44 -0300

    criação do UsuarioBO

--- src/Model/BO/BaseInterBO.java
+++ src/Model/BO/BaseInterBO.java
@@ -1,10 +1,13 @@
 package Model.BO;
 
+import java.sql.SQLException;
 import java.util.*;
 
+import Model.Exception.InsertException;
+
 public interface BaseInterBO <vo> {
-    public List<vo> Buscar(vo entity);
-    public void Cadastrar(vo entity);
-    public void Editar(vo entity);
-    public void Excluir(vo entity);
+    public List<vo> Buscar(vo vo) throws SQLException;
+    public void Cadastrar(vo vo) throws SQLException;
+    public void Editar(vo vo) throws SQLException;
+    public void Excluir(vo vo) throws SQLException;
 }
