2ef0522085f9f96ef92be572d1c22750f61dfdd3
LucasPaulinoH <lucashsp12@hotmail.com>
2021-10-21 15:13:26 -0300

    Merge branch 'main' of https://github.com/jhoshkadash/cuidare

--- src/controller/frontController.java
+++ src/controller/frontController.java
@@ -1,5 +1,14 @@
-package controller;
+package Controller;
 
-public class frontController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class FrontController {
     
+    @FXML private TextField login;
+    @FXML private TextField senha;
+    public void autenticar(ActionEvent event) {
+
+    }
 }
