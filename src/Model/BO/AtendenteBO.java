53bfec4ad0d6dd7a5b480b92f2c742ce1a9ce2a5
João Neto <jnetogoncalo@gmail.com>
2021-11-16 14:15:21 -0300

    att

--- src/Model/BO/AtendenteBO.java
+++ src/Model/BO/AtendenteBO.java
@@ -1,9 +1,16 @@
 package Model.BO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
 import Model.DAO.PacienteDAO;
 import Model.Exception.InsertException;
+import Model.Exception.ListException;
 import Model.VO.AtendenteVO;
+import Model.VO.ConsultaVO;
 import Model.VO.PacienteVO;
+import javafx.scene.chart.PieChart;
 
 public class AtendenteBO{
 
@@ -24,14 +31,72 @@ public class AtendenteBO{
                 throw new InsertException("Paciente com endereço vazio");
             } 
             else {
-                try {
                     PacienteDAO dao = new PacienteDAO();
-                    dao.Inserir(vo);
-                } catch (Exception e) {
-                    e.getMessage();
+                    ResultSet rs;
+                    try {
+                        rs = dao.ListarPorNome(vo);
+                        if(rs.next()){
+                            throw new InsertException("Paciente com mesmo nome já cadastrado");
+                        } else{
+                            dao.Inserir(vo);
+                        }
+                    } catch (SQLException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
                 }
             } 
     
+    public void MarcarConsulta (ConsultaVO vo) throws InsertException {
+        if(vo.getDataConsulta() == null){
+          throw new InsertException("Data não informada");
+        }
+        if(vo.getIdMedico() == null){
+            throw new InsertException("Médico não definido");
+        }
+        if(vo.getIdPaciente() == null){
+            throw new InsertException("Paciente não definido");
+        }
+
+
+    }
+
+
+    public List <PacienteVO> BuscarPacientePorCPF (PacienteVO vo) throws ListException {
+        if(vo.getCpf().length() != 11){ // verificando se a string de cpf possui 11 digitos
+            throw new ListException("CPF inválido, não possui 11 digitos, não escreva pontos e nem linhas");
+        }
+        if(vo.getCpf() == null){ // verificando se existe dados dentro do cpf
+            throw new ListException("CPF está vazio");
+        }
+        if(vo.getCpf().matches("^[0-9]*$") == false){ // verificando se só contem números no cpf
+            throw new ListException("CPF só pode conter números");
+        }
+        else{ // metodo de busca
+            ResultSet rs;
+            List <PacienteVO> pacientes = new ArrayList<PacienteVO>();
+            PacienteDAO dao = new PacienteDAO();
+            try {
+                rs = dao.ListarPorCpf(vo);
+                if(rs.next()){
+                    while(rs.next()){
+                        PacienteVO pacienteVO = new PacienteVO();
+                        pacienteVO.setCpf(rs.getString("CPF"));
+                        pacienteVO.setEndereco(rs.getString("endereco"));
+                        pacienteVO.setNome(rs.getString("nome"));
+                        pacienteVO.setIdPaciente(rs.getLong("id_paciente"));
+                        pacienteVO.setIdPessoa(rs.getLong("id_paciente_pessoa"));
+                        pacientes.add(pacienteVO);
+                    }
+                }else{
+                    throw new ListException("Não foi encontrado nenhum resultado para a busca");
+                }
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return pacientes;
+        }
     }
 
     public void Editar(AtendenteVO vo) {
--- src/Model/BO/testeBO.java
+++ src/Model/BO/testeBO.java
@@ -1,17 +1,28 @@
 package Model.BO;
 
+import java.util.List;
+
 import Model.Exception.InsertException;
+import Model.Exception.ListException;
 import Model.VO.PacienteVO;
 
 public class testeBO {
     public static void main(String[] args) {
-        PacienteVO vo  = new PacienteVO("", "34592308476", "Argentina");
+        PacienteVO vo  = new PacienteVO("", "84593402394", "");
         AtendenteBO bo = new AtendenteBO();
-        PacienteVO vo2 = new PacienteVO();
         try {
-            bo.CadastrarPaciente(vo);
-        } catch (InsertException e) {
-            System.out.println(e.getMessage());
+            List <PacienteVO> pacientes = bo.BuscarPacientePorCPF(vo);
+            for (PacienteVO aux : pacientes ){
+                System.out.println("Nome do paciente : " + aux.getNome());
+                System.out.println("CPf do paciente : " + aux.getCpf());
+                System.out.println("Endereço do paciente : " + aux.getEndereco());
+                System.out.println("Id do paciente : " + aux.getIdPaciente());
+                System.out.println("Id de pessoa : " + aux.getIdPessoa());
+            }
+        } catch (ListException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+        
     }
 }
