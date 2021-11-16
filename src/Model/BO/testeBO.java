d5f6bcaff5a2d8ada900b5efe0601521759e7227
João Neto <jnetogoncalo@gmail.com>
2021-11-16 12:51:35 -0300

    att

--- src/Model/BO/testeBO.java
+++ src/Model/BO/testeBO.java
@@ -7,6 +7,7 @@ public class testeBO {
     public static void main(String[] args) {
         PacienteVO vo  = new PacienteVO("", "34592308476", "Argentina");
         AtendenteBO bo = new AtendenteBO();
+        PacienteVO vo2 = new PacienteVO();
         try {
             bo.CadastrarPaciente(vo);
         } catch (InsertException e) {
